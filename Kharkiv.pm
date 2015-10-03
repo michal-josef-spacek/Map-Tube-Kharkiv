1039bd502a3229d5b33e1ae887a8b360b4321a91
Michal Špaček <tupinek@gmail.com>
2015-10-03 18:12:37 +0200

    Add EXAMPLE4 in doc.

--- Kharkiv.pm
+++ Kharkiv.pm
@@ -197,6 +197,30 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Kharkiv;
+
+ # Object.
+ my $obj = Map::Tube::Kharkiv->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Олексіївська лінія
+ # Салтівська лінія
+ # Холодногірсько-Заводська лінія
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
