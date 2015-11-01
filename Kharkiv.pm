71eaa8679e3ecf34b57dace4906fbb65538345c7
Michal Špaček <tupinek@gmail.com>
2015-11-01 19:47:46 +0100

    Add EXAMPLE5 section to doc.

--- Kharkiv.pm
+++ Kharkiv.pm
@@ -221,6 +221,48 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
  # Салтівська лінія
  # Холодногірсько-Заводська лінія
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(decode_utf8 encode_utf8);
+ use Map::Tube::Kharkiv;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = decode_utf8($ARGV[0]);
+
+ # Object.
+ my $obj = Map::Tube::Kharkiv->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Олексіївська лінія' argument.
+ # Олексіївська
+ # 23 Серпня
+ # Ботанічний сад
+ # Наукова
+ # Держпром
+ # Архітектора Бекетова
+ # Площа Повстання
+ # Метробудівників імені Ващенка
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
