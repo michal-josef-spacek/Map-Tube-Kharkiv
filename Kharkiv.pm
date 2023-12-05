7e5311f0490c2b8cd15c1a1f6c4dca543f6f8f68
Michal Josef Špaček <tupinek@gmail.com>
2023-12-05 15:22:19 +0100

    Remove simple comments

--- Kharkiv.pm
+++ Kharkiv.pm
@@ -1,16 +1,13 @@
 package Map::Tube::Kharkiv;
 
-# Pragmas.
 use strict;
 use warnings;
 use 5.006;
 
-# Modules.
 use File::Share ':all';
 use Moo;
 use namespace::clean;
 
-# Version.
 our $VERSION = 0.07;
 
 # Get XML.
@@ -117,11 +114,9 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE1
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(decode_utf8 encode_utf8);
  use Map::Tube::Kharkiv;
 
@@ -139,12 +134,10 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE2
 
- # Pragmas.
  use strict;
  use utf8;
  use warnings;
 
- # Modules.
  use Map::Tube::Kharkiv;
 
  # Object.
@@ -161,11 +154,9 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE3
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::GraphViz;
  use Map::Tube::GraphViz::Utils qw(node_color_without_label);
  use Map::Tube::Kharkiv;
@@ -199,11 +190,9 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE4
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(encode_utf8);
  use Map::Tube::Kharkiv;
 
@@ -223,11 +212,9 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE5
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(decode_utf8 encode_utf8);
  use Map::Tube::Kharkiv;
 
