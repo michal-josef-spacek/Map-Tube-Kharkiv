c7636ead4ea0cc21a472db6f962d5366345d67fc
Michal Josef Špaček <tupinek@gmail.com>
2023-12-05 15:24:38 +0100

    Update author github username

--- Kharkiv.pm
+++ Kharkiv.pm
@@ -182,8 +182,8 @@ For more information about Kharkiv Map, click L<here|https://en.wikipedia.org/wi
 
 =begin html
 
-<a href="https://raw.githubusercontent.com/tupinek/Map-Tube-Kharkiv/master/images/Kharkiv.png">
-  <img src="https://raw.githubusercontent.com/tupinek/Map-Tube-Kharkiv/master/images/Kharkiv.png" alt="Харківський метрополітен" width="300px" height="300px" />
+<a href="https://raw.githubusercontent.com/michal-josef-spacek/Map-Tube-Kharkiv/master/images/Kharkiv.png">
+  <img src="https://raw.githubusercontent.com/michal-josef-spacek/Map-Tube-Kharkiv/master/images/Kharkiv.png" alt="Харківський метрополітен" width="300px" height="300px" />
 </a>
 
 =end html
@@ -277,7 +277,7 @@ Install the Map::Tube concrete metro modules.
 
 =head1 REPOSITORY
 
-L<https://github.com/tupinek/Map-Tube-Kharkiv>
+L<https://github.com/michal-josef-spacek/Map-Tube-Kharkiv>
 
 =head1 AUTHOR
 
